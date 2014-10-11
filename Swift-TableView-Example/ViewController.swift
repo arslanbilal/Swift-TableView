f8a6249d69765ca5279b40fa2be726a567cf9f39
Bilal <bilal.arslan1993@hotmail.com>
2014-10-11 19:44:31 +0300

    Image files added and cell image design setted

--- Swift-TableView-Example/ViewController.swift
+++ Swift-TableView-Example/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     
     @IBOutlet var tableView: UITableView?
     let recipes: [String] = ["Egg Benedict", "Mushroom Risotto", "Full Breakfast", "Hamburger", "Ham and Egg Sandwich", "Creme Brelee", "White Chocolate Donut", "Starbucks Coffee", "Vegetable Curry", "Instant Noodle with Egg", "Noodle with BBQ Pork", "Japanese Noodle with Pork", "Green Tea", "Thai Shrimp Cake", "Angry Birds Cake", "Ham and Cheese Panini"]
+    let thumbnails = ["egg_benedict.jpg", "mushroom_risotto.jpg", "full_breakfast.jpg", "hamburger.jpg", "ham_and_egg_sandwich.jpg", "creme_brelee.jpg", "white_chocolate_donut.jpg", "starbucks_coffee.jpg", "vegetable_curry.jpg", "instant_noodle_with_egg.jpg", "noodle_with_bbq_pork.jpg", "japanese_noodle_with_pork.jpg", "green_tea.jpg", "thai_shrimp_cake.jpg", "angry_birds_cake.jpg", "ham_and_cheese_panini.jpg"]
+    
  
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +31,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         var cell: UITableViewCell = UITableViewCell()
         
         cell.textLabel?.text = recipes[indexPath.row]
-        cell.imageView?.image = UIImage(named: "Users-Cook-icon.png")
-        
+        cell.imageView?.image = UIImage(named:thumbnails[indexPath.row])
         
         return cell
     }
