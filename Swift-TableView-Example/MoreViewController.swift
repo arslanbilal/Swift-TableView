4977b7ac718ed004255de371821d29e0266fd090
Bilal <bilal.arslan1993@hotmail.com>
2014-10-12 13:16:45 +0300

    WebView Added

---
+++ Swift-TableView-Example/MoreViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MoreViewController.swift
+//  Swift-TableView-Example
+//
+//  Created by Bilal ARSLAN on 12/10/14.
+//  Copyright (c) 2014 Bilal ARSLAN. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MoreViewController: UIViewController {
+    
+    @IBOutlet var webView: UIWebView?
+    
+    override func viewDidLoad() {
+        
+        var url: NSURL = NSURL(string: "https://github.com/ArslanBilal")
+        var urlRequest: NSURLRequest = NSURLRequest(URL: url)
+        webView!.loadRequest(urlRequest)
+    }
+}
